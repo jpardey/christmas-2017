d088081992935826b8b27528d06346fd0ef65dea
Jimmy Pardey <jpardey@gmail.com>
2017-11-12 17:47:37 -0800

    Changed coordinates, added mirroring and rotations and stuff, kind of works

--- snowflake.js
+++ snowflake.js
@@ -12,6 +12,28 @@ function genPoint(n){
     return li;
 }
 
+//Get new points array mirrored around the 30 deg CW axis 
+function getMirrored30(points) {
+    var i;
+    mpoints = [];
+    for (i=0; i<points.length; ++i) {
+        var point = points[i]; //Rotate 60 CCW, then mirror around Y axis
+        mpoints.push([- point[0]/2 + 0.8660254037844387*point[1], 0.8660254037844387*point[0] + point[1]/2]);
+    }   
+    return mpoints;
+}
+
+//Rotate points around 60 deg axis
+function getRotated60(points) {
+    var i;
+    rpoints = [];
+    for (i=0; i<points.length; ++i) {
+        var point = points[i]; //Rotate 60 CCW
+        rpoints.push([ point[0]/2 - 0.8660254037844387*point[1], 0.8660254037844387*point[0] + point[1]/2]);
+    }   
+    return rpoints;
+}
+
 
 //is point d in circumcircle defined by a, b, c?
 function inCircumcircle(a, b, c, d) {
@@ -32,6 +54,7 @@ function inCircumcircle(a, b, c, d) {
     var cd2 = cdy*cdy + cdx*cdx;
 
     var detn = adx*bdy*cd2 + cdx*ady*bd2 + bdx*cdy*ad2 - adx*cdy*bd2 - bdx*ady*cd2 - cdx*bdy*ad2;
+    //console.log(detn);
     return (ccw > 0) ? (detn > 0) : (detn < 0);
 }
 
@@ -64,6 +87,8 @@ function Triangulation(ma, mb, mc) {
         outer: for (i = 0; i<triangles.length; ++i) {
             for (j=0; j<3; ++j) {
                 if (triangles[i][j] < 3) continue outer;
+                //TODO: This is wrong. Ends up with concave exteriors in some cases.
+                // Removing a corner may need to add new edge between points on hull
             } 
             realTriangles.push(triangles[i]);
         } 
@@ -80,6 +105,7 @@ function Triangulation(ma, mb, mc) {
                 var a = triangles[j][k];
                 var b = triangles[j][(k+1)%3]; //Next node
                 if ((a < 3) || (b<3)) continue;
+                //TODO: This is wrong. See getTriangulation
                 var edgestr = (a>b)? "e"+a+","+b: "e"+b+","+a; //Unique and identical string for each edge, orientation independent.
                     //Might be slow, but seems fast enough for our purposes.
                 edges[edgestr] = [a,b];  //Add verticies as value to reduce string ops slightly.
@@ -88,6 +114,23 @@ function Triangulation(ma, mb, mc) {
         return Object.values(edges);
     }
     
+    var getFullEdges = function () {
+        var j;
+        var edges={};
+        for (j=0;j<triangles.length;++j) {
+            var k;
+            for (k = 0; k<3; ++k) {
+                var a = triangles[j][k];
+                var b = triangles[j][(k+1)%3]; //Next node
+               // if ((a < 3) || (b<3)) continue;
+                var edgestr = (a>b)? "e"+a+","+b: "e"+b+","+a; //Unique and identical string for each edge, orientation independent.
+                    //Might be slow, but seems fast enough for our purposes.
+                edges[edgestr] = [a,b];  //Add verticies as value to reduce string ops slightly.
+            } 
+        }
+        return Object.values(edges);
+    }
+
     function compareFunc(a,b) {
         if (a[0] > b[0]) return 1;
         else if (a[0] === b[0]) return 0;
@@ -108,8 +151,6 @@ function Triangulation(ma, mb, mc) {
             weights[i] = [(sq(points[edges[i][0]][0]-points[edges[i][1]][0]) + sq(points[edges[i][0]][1]-points[edges[i][1]][1])), i, edges[i]]
         } 
         weights.sort(compareFunc);
-        console.log(weights);
-        console.log(edges);
         var treeEdges = new Set(); //set of edge numbers
         var treeVerts = new Set([3]); //Pick "random" vertex
 
@@ -152,6 +193,13 @@ function Triangulation(ma, mb, mc) {
         return points;
     }
     //Add points to the actual triangulation
+
+    var addPoints = function(li) {
+        var i;
+        for (i=0; i<li.length; ++i) {
+            addPoint(li[i]);
+        }
+    }
 
     //WARNING: Overlapping points may not be added properly.
     var addPoint = function(point) { 
@@ -205,8 +253,10 @@ function Triangulation(ma, mb, mc) {
 
     return {
         "addPoint":addPoint, 
+        "addPoints":addPoints, 
         "getPoints":getPoints, 
         "getEdges":getEdges, 
+        "getFullEdges":getFullEdges, 
         "getTriangulation":getTriangulation, 
         "getFullTriangulation":getFullTriangulation, 
         "getMST":getMST
@@ -216,7 +266,61 @@ function Triangulation(ma, mb, mc) {
 
 function sq(x) {return x*x}
 
-triangulation = Triangulation([0,0],[0,500],[500,0]);
+triangulation = Triangulation([-2000,-2000],[-2000,15000],[15000,-2000]);
+
+//Take a triangulation and draw it
+var testDraw = function(t) {
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    var edges = t.getEdges();
+    var points = t.getPoints();
+    var mirrored = getMirrored30(points);
+
+    var allPoints = [];
+    allPoints.push(points);
+    allPoints.push(mirrored);
+    var i;
+    for (i = 2; i < 6; ++i) {
+        allPoints.push(getRotated60(allPoints[i-2]));
+    }
+
+    var MST = t.getMST();
+    var extraEdgeList = pickFlakeEdges(t);
+    console.log(extraEdgeList);
+    var extraEdges = new Set(extraEdgeList);
+    var l;
+    var a,b;
+    for (l = 0; l<edges.length; ++l) {
+        if (MST.has(l)) {
+            ctx.strokeStyle="#00FF00";
+            ctx.lineWidth=2.5;
+        }
+        else if (extraEdges.has(l)) {
+            ctx.strokeStyle = "#DD00DD" 
+            ctx.lineWidth=2.5;
+        }
+        else {
+            ctx.strokeStyle =  "#CCCCCC";
+            ctx.lineWidth=1.0;
+        }
+        for(i=0;i<6;++i) {
+            a = allPoints[i][edges[l][0]];
+            b = allPoints[i][edges[l][1]];
+            ctx.beginPath();
+            ctx.moveTo(a[0]*250 + 500, a[1]*250 + 500);
+            ctx.lineTo(b[0]*250 + 500, b[1]*250 + 500);
+            ctx.stroke();
+        }
+    }
+    ctx.lineWidth=1.0;
+    ctx.fillStyle = "#FF0000";
+    ctx.lineStyle =  "#FF0000";
+    for (l = 3; l<points.length; ++l) {
+        ctx.beginPath();
+        ctx.arc(points[l][0], points[l][1], 3, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+
+}
 
 var addAndDraw = function(ev) {
     if (ev) {
@@ -224,7 +328,7 @@ var addAndDraw = function(ev) {
         else triangulation.addPoint([ev.layerX, ev.layerY]); 
     }
     ctx.clearRect(0,0,canvas.width, canvas.height);
-    var edges = triangulation.getEdges();
+    var edges = triangulation.getFullEdges();
     var points = triangulation.getPoints();
     var MST = triangulation.getMST();
     var l;
@@ -253,6 +357,36 @@ var addAndDraw = function(ev) {
         ctx.arc(points[l][0], points[l][1], 3, 0, 2 * Math.PI);
         ctx.fill();
     }
+    //TESTING remove
+    var leftmost = 3;
+    var rightmost = 3;
+    var innermost = 3;
+    var rightmostDist = sliceCos*points[3][0] + sliceSin*points[3][1];
+    for (i=4; i<points.length; ++i ) {
+        if (points[i][0] < points[leftmost][0]) {
+            leftmost = i;
+        }
+        //Lazy. Whatever.
+        if (points[i][1] > points[innermost][1]) {
+            innermost = i;
+        }
+        var dist = points[i][0]*sliceCos + points[i][1]*sliceSin;
+        if (dist > rightmostDist) {
+            rightmost = i;
+            rightmostDist = dist;
+        }
+    }
+    ctx.fillStyle = "#0000FF";
+    ctx.lineStyle =  "#0000FF";
+        ctx.beginPath();
+        ctx.arc(points[rightmost][0], points[rightmost][1], 5, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.arc(points[leftmost][0], points[leftmost][1], 5, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.arc(points[innermost][0], points[innermost][1], 5, 0, 2 * Math.PI);
+        ctx.fill();
     //console.log("edges: " + edges.length);
     //console.log("points: " + points.length);
 }
@@ -262,7 +396,12 @@ var sliceSin = Math.sin(theta);
 var sliceCos = Math.cos(theta);
 var sliceTan = Math.tan(theta);
 var upperSlope = (1-sliceCos)/sliceSin;
-function genSnowflakePoints(n, factor) {
+
+//n: number of points within slice
+//center: bool, draw center point?
+//right: bool, draw right point?
+//left: bool, draw left point?
+var genSnowflakePoints = function(n,center,right,left) {
     var li = [];
     var i;
     var p;
@@ -270,26 +409,91 @@ function genSnowflakePoints(n, factor) {
     for (i = 0; i<n; ++i) {
         do {
             p = [Math.random(), Math.random()];
-            if (p[0] + p[1] > 1) {p = [1-p[0], 1-p[1]];}
+            if (p[0] > p[1]) {p = [p[1], p[0]];}
             p = [p[0]*sliceTan, p[1]];
-        } while ((p[0] > sliceSin) || p[1]<upperSlope*p[0]);
-        p = [Math.round(p[0]*factor), Math.round(p[1]*factor)];
+        } while ((p[0] > sliceSin) || p[1] < 0.1 || p[1]>1-upperSlope*p[0]);
         li.push(p);
-    //    triangulation.addPoint(p);
-    //    addAndDraw();
     }
+
+    if (center) {
+        li.push([1,1]);
+    }
+
+    if (left) {
+        li.push([0,Math.random()]);
+    }
+
+    if (right) {
+        var y = /*(1-sliceCos)+*/sliceCos*Math.random();
+        li.push([Math.round((y)*sliceTan),Math.round(y)])
+    }
+
+
+    
+
     return li;
 }
 
-canvas.addEventListener("click", addAndDraw);
+//Inputs: nothing
+//Outputs: triangulation object (TODO: Should it be global, or generated, or optionally passed in?)
+var genSnowflakeMesh = function() {
+    //First, pick a number of points between 5 and 10
+    var n = 5+Math.floor(Math.random()*6);
+    //Make that many points. Render it as a 512 x 512 (Later. Set factor to 256 for that. Might be a bit wider or narrower too?) 
+    var s = Math.floor(Math.random()*4)
+    var cen_p = 1, lef_p = 1, rig_p = 1;
+    switch (s){
+        case 1: cen_p = 0; break;
+        case 2: lef_p = 0; break;
+        case 3: rig_p = 0; break;
+            
+    }
 
+    var points = genSnowflakePoints(n,cen_p,rig_p,lef_p);
+    var i;
+
+    //var t = Triangulation([0,0], [500,0], [0,500]);
+    var t = triangulation;
+
+    t.addPoints(points); 
+    
+    points = t.getPoints(); //Probably not necessary, but ensures indexing identical
+    return t;
+}
+
+var pickFlakeEdges = function(t) {
+    var extraEdges = [];
+    var chosenEdges = [];
+
+    var allEdges = t.getEdges(); //Returns an array
+    var mstEdges = t.getMST(); //Returns a set. Inconsistent, but it works here.
+    var i;
+    for (i=0; i<allEdges.length; ++i) {
+        if (!mstEdges.has(i)){
+            extraEdges.push(i);
+        }
+    }
+    console.log(extraEdges);
+    var n = extraEdges.length;
+    for (i = 0; i<n; ++i) {
+        //Pick approx 10% of edges not in MST
+        if (Math.random() < 0.1) {chosenEdges.push(extraEdges[i]);}
+    }
+
+    return chosenEdges;
+}
+
+testDraw(genSnowflakeMesh());
+
+//canvas.addEventListener("click", addAndDraw);
+/*
 var li = genSnowflakePoints(500, 500);
 var i;
 for (i = 0; i<li.length;++i) {
     triangulation.addPoint(li[i]);
 }
 addAndDraw();
-
+*/
 function circletest() {
     var canvas = document.getElementById("dotdemo");
     var ctx = canvas.getContext("2d");
